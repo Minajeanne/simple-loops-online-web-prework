dd13a0c792fb670a42aff99b38dceb4a24e32948
Alicia Santiago <albrown07@mac.com>
2019-02-08 13:56:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,6 +4,7 @@ def loop_iterator(number_of_times)
   loop do 
     puts phrase
       break if number_of_times == 7
+      number_of_times = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
   end
 end
